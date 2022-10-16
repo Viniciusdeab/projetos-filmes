96068b675029be75706f0f8de82afbaaac7116a6
Vinicius Brito <viniciusdeab@outlook.com>
2022-10-16 18:35:37 -0300

    Estilização do intens de pesquisa e criação da saida para os melhores filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default Home
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Movie = () => {
+
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+
+  };
+
+  useEffect(()=>{
+
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedURL);
+
+  }, [])
+
+
+  return (
+    <div className="container">
+        <h2 className="title">Melhores filmes</h2>
+        <div className="movies-container">
+        {topMovies && topMovies.map((movie)=> <p>{movie.title}</p>)}
+        </div>
+    </div>
+  );
+};
+
+export default Movie
